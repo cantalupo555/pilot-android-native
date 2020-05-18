4d4fb107bdf24264fa8f262de6edee52e65049b3
cantalupo555 <cantalupo@protonmail.com>
2020-05-18 17:12:36 -0300

    Update

---
+++ Netflix/app/src/main/java/com/cantalupo/netflix/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cantalupo.netflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
