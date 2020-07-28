18f60fa3d311d807a0d25725977a11a77c1b4532
cantalupo555 <cantalupo@protonmail.com>
2020-07-28 03:34:13 -0300

    Update

--- FrasesdoDia/app/src/main/java/com/cantalupo/frasesdodia/MainActivity.java
+++ FrasesdoDia/app/src/main/java/com/cantalupo/frasesdodia/MainActivity.java
@@ -3,6 +3,10 @@ package com.cantalupo.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+
+        String[] frases = {
+          "Frase 01",
+          "Frase 02",
+          "Frase 03",
+          "Frase 04",
+          "Frase 05"
+        };
+
+        int numero = new Random().nextInt(4);
+
+        TextView texto = findViewById(R.id.textView);
+        texto.setText(frases[numero]);
+
     }
 }
